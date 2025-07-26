ced4fe4355ad78c7c8598020cd68b34a5f1f4dc4
Prashuk Jain <29jainprashuk@gmail.com>
2025-07-26 13:01:09 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3200; // Choose only one port
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server Running on port ${PORT}`);
+});
