88a8cfd64d9179bcb6d71e028c2ebba56265dd16
Manish Dangi <manish.dangi@strideone.in>
2025-07-26 14:06:25 +0530

    group message controller

---
+++ backend/src/models/group.message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const groupMessageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    groupId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const GroupMessage = mongoose.model("GroupMessage", groupMessageSchema);
+
+export default GroupMessage;
